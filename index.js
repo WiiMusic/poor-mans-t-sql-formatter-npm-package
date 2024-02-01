ba916870d5d9942d2ce90d3f6beeebe251d3a747
Kyllian Régis <regis.kyllian@stas.com>
2024-02-01 14:13:29 -0500

    Fix : Properly get the option config

--- index.js
+++ index.js
@@ -31085,7 +31085,7 @@ Bridge.assembly("PoorMansTSqlFormatterJS", function ($asm, globals) {
                 case PoorMansTSqlFormatterLib.Interfaces.SqlStructureConstants.ENAME_MONETARY_VALUE: 
                 case PoorMansTSqlFormatterLib.Interfaces.SqlStructureConstants.ENAME_LABEL: 
                     this.WhiteSpace_SeparateWords(state);
-                    if (PoorMansTSqlFormatterLib.Formatters.TSqlStandardFormatterOptions.AddBracketsAroundNames && !contentElement.PoorMansTSqlFormatterLib$ParseStructure$Node$TextValue.startsWith("@"))
+                    if (this.Options.AddBracketsAroundNames && !contentElement.PoorMansTSqlFormatterLib$ParseStructure$Node$TextValue.startsWith("@"))
                         state.AddOutputContent("[" + contentElement.PoorMansTSqlFormatterLib$ParseStructure$Node$TextValue + "]");
                     else
                         state.AddOutputContent(contentElement.PoorMansTSqlFormatterLib$ParseStructure$Node$TextValue);
